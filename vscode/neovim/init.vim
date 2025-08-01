3e4990f9e3f5f22bcb3aa16ad60aecf73018f8cd
smendest <smendestello@gmail.com>
2025-08-01 17:35:27 -0300

    remove repeated dir

--- vscode/neovim/init.vim
+++
@@ -1,5 +0,0 @@
-set number	
-set splitbelow	
-
-vnoremap <c-t> :split <CR>:ter<CR>:resize 15<CR> 
-nnoremap <c-t> :split <CR>:ter<CR>:resize 15<CR> 
--- vscode/neovim/nvim/lua/plugins/grug-far.lua
+++
@@ -1,8 +0,0 @@
---  Use tag 1.6.3 for nvim 0.10
-return {
-  "MagicDuck/grug-far.nvim",
-  tag = "1.6.3",
-  config = function()
-    require("grug-far").setup({})
-  end,
-}
