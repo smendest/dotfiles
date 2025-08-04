511715d28cc02fbee60f276a58cf588ab0aa8dce
smendest <smendestello@gmail.com>
2025-08-04 13:20:43 -0300

    update colorscheme

--- neovim/nvim/lua/plugins/colorscheme.lua
+++ neovim/nvim/lua/plugins/colorscheme.lua
@@ -3,8 +3,19 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "EdenEast/nightfox.nvim" },
   { "Mofiqul/vscode.nvim" },
+  { "folke/tokyonight.nvim", lazy = true, opts = { style = "night" } },
+  { "rebelot/kanagawa.nvim" },
+  { "navarasu/onedark.nvim", opts = { style = "dark" } },
 
   { "LazyVim/LazyVim", opts = {
     colorscheme = "catppuccin-mocha",
   } },
 }
+
+-- return {
+--   { "Mofiqul/vscode.nvim" },
+--
+--   { "LazyVim/LazyVim", opts = {
+--     colorscheme = "vscode",
+--   } },
+-- }
